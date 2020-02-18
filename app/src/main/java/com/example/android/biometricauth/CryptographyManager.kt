38da94e8e555576f0ccccf128535732453a0cbaf
isaid <isaid@google.com>
2020-02-18 14:44:45 -0800

    Create sample for blog post on BiometricPrompt.CryptoObject

---
+++ app/src/main/java/com/example/android/biometricauth/CryptographyManager.kt
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.example.android.biometricauth
+
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import java.nio.charset.Charset
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.GCMParameterSpec
+
+
+/**
+ * Copyright (C) 2020 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+interface CryptographyManager {
+
+    /**
+     * This method first gets or generates an instance of SecretKey and then initializes the Cipher
+     * with the key. The secret key uses [ENCRYPT_MODE][Cipher.ENCRYPT_MODE] is used.
+     */
+    fun getInitializedCipherForEncryption(keyName: String): Cipher
+
+    /**
+     * This method first gets or generates an instance of SecretKey and then initializes the Cipher
+     * with the key. The secret key uses [DECRYPT_MODE][Cipher.DECRYPT_MODE] is used.
+     */
+    fun getInitializedCipherForDecryption(keyName: String, initializationVector: ByteArray): Cipher
+
+    /**
+     * The Cipher created with [getInitializedCipherForEncryption] is used here
+     */
+    fun encryptData(plaintext: String, cipher: Cipher): ByteArray
+
+    /**
+     * The Cipher created with [getInitializedCipherForDecryption] is used here
+     */
+    fun decryptData(ciphertext: ByteArray, cipher: Cipher): String
+
+}
+
+fun CryptographyManager(): CryptographyManager = CryptographyManagerImpl()
+
+private class CryptographyManagerImpl : CryptographyManager {
+
+    private val KEY_SIZE: Int = 256
+    val ANDROID_KEYSTORE = "AndroidKeyStore"
+    private val ENCRYPTION_BLOCK_MODE = KeyProperties.BLOCK_MODE_GCM
+    private val ENCRYPTION_PADDING = KeyProperties.ENCRYPTION_PADDING_NONE
+    private val ENCRYPTION_ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
+
+    override fun getInitializedCipherForEncryption(keyName: String): Cipher {
+        val cipher = getCipher()
+        val secretKey = getOrCreateSecretKey(keyName)
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+        return cipher
+    }
+
+    override fun getInitializedCipherForDecryption(keyName: String, initializationVector: ByteArray): Cipher {
+        val cipher = getCipher()
+        val secretKey = getOrCreateSecretKey(keyName)
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, GCMParameterSpec(128, initializationVector))
+        return cipher
+    }
+
+    override fun encryptData(plaintext: String, cipher: Cipher): ByteArray {
+        return cipher.doFinal(plaintext.toByteArray(Charset.forName("UTF-8")))
+    }
+
+    override fun decryptData(ciphertext: ByteArray, cipher: Cipher): String {
+        val plaintext = cipher.doFinal(ciphertext)
+        return String(plaintext, Charset.forName("UTF-8"))
+    }
+
+    private fun getCipher(): Cipher {
+        val transformation = "$ENCRYPTION_ALGORITHM/$ENCRYPTION_BLOCK_MODE/$ENCRYPTION_PADDING"
+        return Cipher.getInstance(transformation)
+    }
+
+    private fun getOrCreateSecretKey(keyName: String): SecretKey {
+        // If Secretkey was previously created for that keyName, then grab and return it.
+        val keyStore = KeyStore.getInstance(ANDROID_KEYSTORE)
+        keyStore.load(null) // Keystore must be loaded before it can be accessed
+        keyStore.getKey(keyName, null)?.let { return it as SecretKey }
+
+        // if you reach here, then a new SecretKey must be generated for that keyName
+        val paramsBuilder = KeyGenParameterSpec.Builder(keyName,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+        paramsBuilder.apply {
+            setBlockModes(ENCRYPTION_BLOCK_MODE)
+            setEncryptionPaddings(ENCRYPTION_PADDING)
+            setKeySize(KEY_SIZE)
+            setUserAuthenticationRequired(true)
+        }
+
+        val keyGenParams = paramsBuilder.build()
+        val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES,
+                ANDROID_KEYSTORE)
+        keyGenerator.init(keyGenParams)
+        return keyGenerator.generateKey()
+    }
+
+}
---
+++ app/src/main/java/com/example/android/biometricauth/MainActivity.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.example.android.biometricauth
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import java.nio.charset.Charset
+
+/**
+ * Main entry point for the sample, showing a backpack and "Purchase" button.
+ * 1. App has an EditView for user to enter text to be encrypted; a button to encrypt; a button
+ * to decrypt; and a TextView to view encrypted/decrypted text.
+ *
+ * 2. When user opens app the EditText is non-empty but the TextView is empty
+ * 3. User clicks encrypt/decrypt to make text appear on TextView.
+ * 4. Option to login with password still exists
+ *
+ */
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textInputView: AppCompatEditText
+    private lateinit var textOutputView: AppCompatTextView
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+    private var readyToEncrypt: Boolean = false
+    private lateinit var cryptographyManager: CryptographyManager
+    private lateinit var secretKeyName: String
+    private lateinit var ciphertext:ByteArray
+    private lateinit var initializationVector: ByteArray
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        cryptographyManager = CryptographyManager()
+        // e.g. secretKeyName = "biometric_sample_encryption_key"
+        secretKeyName = getString(R.string.secret_key_name)
+        biometricPrompt = createBiometricPrompt()
+        promptInfo = createPromptInfo()
+
+        textInputView = findViewById(R.id.input_view)
+        textOutputView = findViewById(R.id.output_view)
+        findViewById<Button>(R.id.encrypt_button).setOnClickListener { authenticateToEncrypt() }
+        findViewById<Button>(R.id.decrypt_button).setOnClickListener { authenticateToDecrypt() }
+    }
+
+    private fun createBiometricPrompt(): BiometricPrompt {
+        val executor = ContextCompat.getMainExecutor(this)
+
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Log.d(TAG, "$errorCode :: $errString")
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Log.d(TAG, "Authentication failed for an unknown reason")
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Log.d(TAG, "Authentication was successful")
+                processData(result.cryptoObject)
+            }
+        }
+
+        //The API requires the client/Activity context for displaying the prompt view
+        val biometricPrompt = BiometricPrompt(this, executor, callback)
+        return biometricPrompt
+    }
+
+    private fun createPromptInfo(): BiometricPrompt.PromptInfo {
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle(getString(R.string.prompt_info_title)) // e.g. "Sign in"
+                .setSubtitle(getString(R.string.prompt_info_subtitle)) // e.g. "Biometric for My App"
+                .setDescription(getString(R.string.prompt_info_description)) // e.g. "Confirm biometric to continue"
+                .setConfirmationRequired(false)
+                .setNegativeButtonText(getString(R.string.prompt_info_use_app_password)) // e.g. "Use Account Password"
+                // .setDeviceCredentialAllowed(true) // Allow PIN/pattern/password authentication.
+                // Also note that setDeviceCredentialAllowed and setNegativeButtonText are
+                // incompatible so that if you uncomment one you must comment out the other
+                .build()
+        return promptInfo
+    }
+
+    private fun authenticateToEncrypt() {
+        readyToEncrypt = true
+        if (BiometricManager.from(applicationContext).canAuthenticate() == BiometricManager
+                        .BIOMETRIC_SUCCESS) {
+            val cipher = cryptographyManager.getInitializedCipherForEncryption(secretKeyName)
+            initializationVector = cipher.iv
+            biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
+        }
+    }
+
+    private fun authenticateToDecrypt() {
+        readyToEncrypt = false
+        if (BiometricManager.from(applicationContext).canAuthenticate() == BiometricManager
+                        .BIOMETRIC_SUCCESS) {
+            val cipher = cryptographyManager.getInitializedCipherForDecryption(secretKeyName,initializationVector)
+            biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
+        }
+
+    }
+
+    private fun processData(cryptoObject: BiometricPrompt.CryptoObject?) {
+        val data = if (readyToEncrypt) {
+            val text = textInputView.text.toString()
+            ciphertext = cryptographyManager.encryptData(text, cryptoObject?.cipher!!)
+            String(ciphertext, Charset.forName("UTF-8"))
+        } else {
+            cryptographyManager.decryptData(ciphertext, cryptoObject?.cipher!!)
+        }
+        textOutputView.text = data
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
